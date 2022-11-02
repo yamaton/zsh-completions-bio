3cbe96cad33772ccfa1d107dd20075ece295fe9b
Yamato Matsuoka <yamaton@gmail.com>
2022-11-02 14:27:30 -0400

    Dump from upstream updates

--- completions/_polca.sh
+++ completions/_polca.sh
@@ -16,9 +16,10 @@ function _polca.sh {
 
     _arguments -C \
         '-a[Assembly contigs or scaffolds]' \
-        '-r[Illumina reads fastq]' \
-        '-m[Memory per thread to use in samtools sort]' \
-        '-t[Number of threads]' \
+        '-r[can use any number of fastq files, polishing reads must be in fastq format!]' \
+        '-t[number of threads (default:1)]' \
+        '-n[(optional) do not polish, just create vcf file, evaluate the assembly and exit]' \
+        '-m[(optional) memory per thread to use in samtools sort, set to 2G or more for large genomes]' \
         '*: :_files'
 
     case $state in
--- completions/_unique-kmers.py
+++ completions/_unique-kmers.py
@@ -1,9 +1,9 @@
-#compdef _trim-low-abund.py trim-low-abund.py
+#compdef _unique-kmers.py unique-kmers.py
 
 # Auto-generated with h2o
 
 
-function _trim-low-abund.py {
+function _unique-kmers.py {
     local line state
 
     function _commands {
@@ -18,25 +18,11 @@ function _trim-low-abund.py {
         '--version[show program'\''s version number and exit]' \
         '--info[print citation information]' \
         {-h,--help}'[show this help message and exit]' \
-        {-k,--ksize}'[k-mer size to use (default: 32)]' \
-        {-U,--unique-kmers}'[approximate number of unique kmers in the input set (default: 0)]' \
-        '--fp-rate[Override the automatic FP rate setting for the current script (default: None)]' \
-        {-M,--max-memory-usage}'[maximum amount of memory to use for data structure (default: None)]' \
-        '--small-count[Reduce memory usage by using a smaller counter for individual kmers. (default: False)]' \
-        {-C,--cutoff}'[remove k-mers below this abundance (default: 2)]' \
-        {-Z,--trim-at-coverage,--normalize-to}'[trim reads when entire read above this coverage (default: 20)]' \
-        {-o,--output}'[only output a single file with the specified filename; use a single dash "-" to specify that output should go to STDOUT (the terminal) (default: None)]':file:_files \
-        {-V,--variable-coverage}'[Only trim low-abundance k-mers from sequences that have high coverage. (default: False)]' \
-        {-l,--loadgraph}'[load a precomputed k-mer graph from disk (default: None)]':file:_files \
-        {-s,--savegraph}'[save the k-mer countgraph to disk after allreads are loaded. (default: )]':file:_files \
-        {-q,--quiet,--summary-info}'[What format should the machine readable run summary be in? (`json` or `tsv`, disabled by default) (default: None)]' \
-        {--force,--ignore-pairs}'[treat all reads as if they were singletons (default: False)]' \
-        {-T,--tempdir}'[Set location of temporary directory for second pass (default: ./)]':file:_files \
-        '--gzip[Compress output using gzip (default: False)]' \
-        '--bzip[Compress output using bzip2 (default: False)]' \
-        '--diginorm[Eliminate high-coverage reads altogether (digital normalization). (default: False)]' \
-        '--diginorm-coverage[Coverage threshold for --diginorm (default: 20)]' \
-        '--single-pass[Do not do a second pass across the low coverage data (default: False)]' \
+        {-q,--quiet,-k,--ksize}'[k-mer size to use (default: 32)]' \
+        {-e,--error-rate}'[Acceptable error rate (default: 0.01)]' \
+        {-R,--report}'[generate informational report and write to filename (default: None)]':file:_files \
+        {-S,--stream-records}'[write input sequences to STDOUT (default: False)]' \
+        '--diagnostics[print out recommended tablesize arguments and restrictions (default: False)]' \
         '*: :_files'
 
     case $state in
